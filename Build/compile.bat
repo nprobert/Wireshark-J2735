c807a511eca02487d2a9a311e2060c906a41c275
Neal Probert <nprobert@probestar.net>
2024-02-29 10:22:30 -0500

    Removed echo

--- Build/compile.bat
+++ Build/compile.bat
@@ -7,8 +7,6 @@ set WIRESHARK_VERSION_EXTRA=-J2735-2024-04
 mkdir wsbuild64
 cd wsbuild64
 
-cmake -G "Visual Studio 17 2022" -A x64 ..\wireshark
-
 msbuild /m /p:Configuration=RelWithDebInfo Wireshark.sln
 
 cd ..

c0b62f906fb62efc3214f4730c432f477bd2a255
Neal Probert <nprobert@probestar.net>
2024-05-31 12:44:06 -0400

    Windows build based on SAE J2735-2024 final ballot version

--- Build/compile.bat
+++ Build/compile.bat
@@ -1,11 +1,13 @@
 set PLATFORM=x64
 set WIRESHARK_TARGET_PLATFORM=x64
-set WIRESHARK_BASE_DIR=%CD%
-set WIRESHARK_QT6_PREFIX_PATH=C:\Qt\6.6.2\msvc2019_64
-set WIRESHARK_VERSION_EXTRA=-J2735-2024-04
+set WIRESHARK_BASE_DIR=D:\Wireshark-J2735\Build
+set WIRESHARK_QT6_PREFIX_PATH=C:\Qt\6.7.0\msvc2019_64
+set WIRESHARK_VERSION_EXTRA=-J2735-20240528
 
 mkdir wsbuild64
 cd wsbuild64
+
+cmake -G "Visual Studio 17 2022" -A x64 ..\wireshark
 
 msbuild /m /p:Configuration=RelWithDebInfo Wireshark.sln
 
